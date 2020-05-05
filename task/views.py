b0276f42f4f219688eefaa1a4696018d72710d9c
Baljeet Jangra <54733764+baljeetjangra@users.noreply.github.com>
2020-05-05 16:42:47 +0530

    task form created and task list shown on web page

--- task/views.py
+++ task/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .forms import TaskForm
+from .models import Task
 # Create your views here.
 
 
 def task_list(request):
-    return render(request, 'task/task_list.html')
+    tasks = Task.objects.all()
+    context = {
+        'form': TaskForm,
+        'tasks': tasks
+    }
+    return render(request, 'task/task_list.html', context)
