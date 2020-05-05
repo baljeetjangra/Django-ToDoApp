bd704e5fb804ff67aa2576c131eafaeafcc31d10
Baljeet Jangra <54733764+baljeetjangra@users.noreply.github.com>
2020-05-05 16:01:37 +0530

    Model created named Task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    task_item = models.CharField(max_length=200)
+    added_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task_item
